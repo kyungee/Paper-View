4617d5c8713492dda3d779c08308b2af69a0f079
홍성표 <sphong0417@gmail.com>
2020-01-12 16:01:43 +0900

    Add Dependency for spring-boot

---
+++ src/main/java/com/hk/paperview/PaperViewApplication.java
@@ -0,0 +1,13 @@
+package com.hk.paperview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hk/paperview/PaperViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hk.paperview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
